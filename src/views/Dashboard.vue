8d6b20325a3db32f5a6b71450b2a8c8b6573d70b
Arachaphon <arachaphonklinchuen@gmail.com>
2025-07-18 22:30:45 +0700

    bo

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,5 +1,7 @@
 <template>
-    <div class="text-green-500">
+    <div class="text-green-500 bg-slate-400 w-screen">
         Welcome to Gravity Plan
+        <i class='bx  bx-building'  ></i> 
     </div>
+    <router-link to="/Home" class="p-10 border-4 border-red-600 mt-6">Go to home</router-link>
 </template>
