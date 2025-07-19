86c6d384110f64a9b20fcf2b7a4ea7a426335b5c
Arachaphon <arachaphonklinchuen@gmail.com>
2025-07-19 21:39:45 +0700

    Taskcontentcreated
    I haven't implemented the task-adding functionality yet, and I haven't connected to the database.

--- src/views/Task/Inbox.vue
+++ src/views/Task/Inbox.vue
@@ -2,11 +2,12 @@
 import NavBar from '@/components/NavBar.vue';
 import TaskBar from '@/components/TaskBar.vue';
 import TaskContenrt from '@/components/TaskContent.vue';
+import emptytask from '@/assets/NoInbox.png';
 </script>
 <template>
-    <body class="w-screen flex flex-row">
+    <div class="w-screen flex flex-row">
         <NavBar />
         <TaskBar />
-        <TaskContenrt title="Inbox" />
-    </body>
+        <TaskContenrt title="Inbox" :emptytask="emptytask" />
+    </div>
 </template>
