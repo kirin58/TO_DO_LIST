8575fd72cec6afb47c980bcc63ad7cb52ddfe1b1
Arachaphon <arachaphonklinchuen@gmail.com>
2025-07-19 16:32:58 +0700

    edit1

---
+++ src/components/TaskContent.vue
@@ -0,0 +1,3 @@
+<template>
+    <div class="w-3/5 bg-green-200">content</div>
+</template>
--- src/views/Task/Inbox.vue
+++ src/views/Task/Inbox.vue
@@ -1,10 +1,12 @@
 <script setup>
 import NavBar from '@/components/NavBar.vue';
 import TaskBar from '@/components/TaskBar.vue';
+import TaskContenrt from '@/components/TaskContent.vue';
 </script>
 <template>
     <body class="w-screen flex flex-row">
         <NavBar />
         <TaskBar />
+        <TaskContenrt />
     </body>
 </template>
