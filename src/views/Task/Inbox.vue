d197f61776018b2203695b649a4df739c8c54716
PT <peepeerapat3105@gmail.com>
2025-07-20 19:17:44 +0700

    11

--- src/views/Task/Inbox.vue
+++ src/views/Task/Inbox.vue
@@ -1,10 +1,13 @@
 <script setup>
 import NavBar from '@/components/NavBar.vue';
 import TaskBar from '@/components/TaskBar.vue';
+import TaskContenrt from '@/components/TaskContent.vue';
+
 </script>
 <template>
     <body class="w-screen flex flex-row">
         <NavBar />
         <TaskBar />
-    </body>
+        <TaskContent title="Inbox" />
+    </div>
 </template>
