30e5b87e546cf84ba8b06be948059524ce854c62
Arachaphon <arachaphonklinchuen@gmail.com>
2025-07-21 22:05:04 +0700

    taskpopup

--- src/components/Task/TaskContent.vue
+++ src/components/Task/TaskContent.vue
@@ -45,42 +45,76 @@ const closePopup = () => {
 }
 </script>
 <template>
-    <div class="w-3/5 min-h-screen">
-        <div class="flex flex-col p-6 ">
-            <div class="flex items-center p-2 mb-2 justify-between">
-                <p class="text-2xl font-black text-stone-600">{{ title }}</p>
-                <div class="w-1/2 flex justify-end gap-4 text-stone-400 text-2xl">
-                    <button><i class='bx  bx-shuffle'  ></i></button>
-                    <button><i class='bx  bx-dots-horizontal'  ></i> </button>
-                </div>
-            </div>
-            <div class="w-full flex items-center bg-zinc-100 rounded-lg p-1 hover:bg-white border border-white focus-within:border-orange-300">
-                <input type="text" v-model="task" @keyup.enter="addTask" placeholder="+ Add task" class="flex-1 bg-transparent text-slate-500 outline-none"/>
-                <input type="date" v-model="dueDate" class="ml-2 text-slate-500 bg-transparent outline-none border-none focus:ring-0" />
-                <i class='bx  bx-chevron-down text-3xl'  ></i> 
-            </div>
+  <div class="w-3/5 min-h-screen">
+    <div class="flex flex-col p-6 ">
+      <div class="flex items-center p-2 mb-2 justify-between">
+        <p class="text-2xl font-black text-stone-600">{{ title }}</p>
+        <div class="w-1/2 flex justify-end gap-4 text-stone-400 text-2xl">
+          <button><i class='bx  bx-shuffle'  ></i></button>
+          <button><i class='bx  bx-dots-horizontal'  ></i> </button>
         </div>
-        <div class="w-full h-5/6 overflow-y-auto p-5 pr-14">
-            <div v-if="tasks.length" >
-              <div v-for="t in tasks":key="t.id"class="flex items-center gap-2">
-                <button><i class='bx  bx-menu text-xl text-zinc-300'  ></i> </button>
-                <button><i class='bx  bx-checkbox text-4xl text-zinc-300'  ></i></button>
-                <div class="w-full flex justify-between p-2 border-b border-gray-300 mb-2 text-black text-lg">
-                  <div>{{ t.text }}</div><div>{{ t.dueDate }}</div>
+      </div>
+      <div class="w-full flex items-center bg-zinc-100 rounded-lg p-1 hover:bg-white border border-white focus-within:border-orange-300">
+        <input type="text" v-model="task" @keyup.enter="addTask" placeholder="+ Add task" class="flex-1 bg-transparent text-slate-500 outline-none"/>
+        <input type="date" v-model="dueDate" class="ml-2 text-slate-500 bg-transparent outline-none border-none focus:ring-0" />
+        <i class='bx  bx-chevron-down text-3xl'  ></i> 
+      </div>
+    </div>
+    <div class="w-full h-5/6 overflow-y-auto p-5 pr-14">
+      <div v-if="tasks.length" >
+        <div v-for="t in tasks":key="t.id"class="flex items-center gap-2">
+          <button><i class='bx  bx-menu text-xl text-zinc-300'  ></i> </button>
+          <button><i class='bx  bx-checkbox text-4xl text-zinc-300'  ></i></button>
+          <div class="w-full flex justify-between p-2 border-b border-gray-300 mb-2 text-black text-lg">
+            <div>{{ t.text }}</div><div>{{ t.dueDate }}</div>
+          </div>
+          <div>
+            <button @click="() => togglePopup(t.id)">
+              <i class='bx  bx-dots-horizontal text-2xl text-zinc-300 ml-3'  ></i>
+            </button>
+            <div v-if="taskMenu === t.id" class="absolute">
+              <div class="absolute top-0 z-[9999] p-2 bg-stone-50 shadow-xl">
+                <div>
+                  <div>Date</div>
+                  <button><i class='bx  bx-sun'  ></i> </button>
+                  <button><i class='bx  bx-calendar-week'  ></i> </button>
+                  <button><i class='bx  bx-calendar-star'  ></i> </button>
+                  <button><i class='bx bx-calendar'  ></i> </button>
                 </div>
                 <div>
-                  <button @click="() => togglePopup(t.id)">
-                    <i class='bx  bx-dots-horizontal text-2xl text-zinc-300 ml-3'  ></i>
-                  </button>
-                  <div v-if="taskMenu === t.id" class="absolute">
-                    <div class="absolute right-0 top-8 z-[9999] bg-red-200 w-36 h-36">menu</div>
-                  </div>
+                  <div>Priority</div>
+                  <button><i class='bx  bx-flag'  ></i></button>
+                  <button><i class='bx  bx-flag'  ></i></button>
+                  <button><i class='bx  bx-flag'  ></i></button>
+                  <button><i class='bx  bx-flag'  ></i></button>
+                </div>
+                <div></div>
+                <div>
+                  <button><i class='bx  bx-pin'  ></i></button>
+                  <p>Pin</p>
+                </div>
+                <div>
+                  <button><i class='bx  bx-chevron-right-square'  ></i> </button>
+                  <p>Move To</p>
+                  <div><button><i class='bx  bxs-chevron-right '></i></button></div>
+                </div>
+                <div>
+                  <button>#</button>
+                  <p>Tags</p>
+                </div>
+                <div></div>
+                <div>
+                  <button><i class='bx  bx-trash'></i></button>
+                  <p>Delete</p>
                 </div>
               </div>
             </div>
-            <div v-else class="flex items-center justify-center h-full">
-                <img :src="emptytask" alt="Empty Inbox" class="w-64 h-auto object-contain opacity-50"/>
-            </div>
+          </div>
         </div>
+      </div>
+          <div v-else class="flex items-center justify-center h-full">
+            <img :src="emptytask" alt="Empty Inbox" class="w-64 h-auto object-contain opacity-50"/>
+          </div>
     </div>
+  </div>
 </template>
