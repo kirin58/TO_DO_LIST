041155d4d260e4f15740c727917a9aa13cefec7e
PT <peepeerapat3105@gmail.com>
2025-07-22 20:53:50 +0700

    icon pomo

--- src/components/PomoLayout.vue
+++ src/components/PomoLayout.vue
@@ -2,6 +2,14 @@
     <div class="w-2/3">
         <div class="flex items-center justify-between px-6 py-8 h-28">
             <div class="font-semibold text-3xl text-stone-800">Pomodoro</div>
+            <div class="flex space-x-4 text-2xl text-stone-400">
+                <button>
+                    <i class='bx  bx-plus'  ></i> 
+                </button>
+                <button>
+                    <i class='bx  bx-dots-horizontal-rounded'  ></i> 
+                </button>
+            </div>
         </div>
         <div class="flex justify-center mt-4 font-bold text-xl ">
             <button class="bg-orange-200 rounded-full px-6 py-2 text-orange-800">Pomo</button>
@@ -159,8 +167,8 @@ export default {
         }
     },
     mounted() {
-        this.minutes = 0;
-        this.seconds = 25;
+        this.minutes = 25;
+        this.seconds = 0;
     }
 };
 </script>
