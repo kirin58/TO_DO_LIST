f1cf581d3795a3e69e9d87c593f6820d611cb8db
Arachaphon <arachaphonklinchuen@gmail.com>
2025-08-31 22:27:10 +0700

    Update TaskContent.vue

--- src/components/Task/TaskContent.vue
+++ src/components/Task/TaskContent.vue
@@ -1,16 +1,15 @@
 <script setup>
-import { ref, onMounted, watch, nextTick } from 'vue'
+import { ref, onMounted, watch, nextTick, computed } from 'vue'
 import TaskPopup from './TaskPopup.vue'
 import draggable from 'vuedraggable'
 import Taskshuffle from './Taskshuffle.vue'
-
-defineProps({
-  emptytask: String
-})
+import { useRoute } from 'vue-router'
 
 const task = ref('')
 const tasks = ref([])
 const completedTasks = ref([])
+const trashTasks = ref([])
+const showCompleted = ref(true)
 const dueDate = ref('')
 const editID = ref(null)
 const editText = ref('')
@@ -23,10 +22,78 @@ const sortByPriority = ref(false)
 const sortByNone = ref(false)
 const popupPosition = ref('bottom')
 
+const props = defineProps({
+  emptytask: { type: String, required: true },
+  empty: String, 
+  emptydis : String,
+  title: String,
+  mode: { type: String, default: 'today' }
+})
+
+const route = useRoute()
+const routeMode = computed(() => {
+  if (props.mode === 'inbox') return 'inbox'
+  if (props.mode === 'next7') return 'next7'
+  return 'today'
+})
+
+const pageTitle = computed(() => {
+  if (props.title) return props.title
+  switch (props.mode) {
+    case 'today': return 'Today'
+    case 'next7': return 'Next 7 Days'
+    case 'inbox': return 'Inbox'
+    case 'completed': return 'Completed'
+    case 'trash': return 'Trash'
+    default: return ''
+  }
+})
+
+const today = new Date()
+today.setHours(0,0,0,0)
+
+const tasksToShow = computed(() => {
+  if (props.mode === 'inbox') {
+    return tasks.value 
+  }
+
+  if (props.mode === 'today') {
+    return tasks.value.filter(t => {
+      if (!t.dueDate) return false
+      const taskDate = new Date(t.dueDate)
+      return taskDate.toDateString() === today.toDateString()
+    })
+  }
+
+  if (props.mode === 'next7') {
+    const start = new Date(today)
+    start.setDate(today.getDate() + 1)
+
+    const end = new Date(today)
+    end.setDate(today.getDate() + 8)
+
+    return tasks.value.filter(t => {
+      if (!t.dueDate) return false
+      const taskDate = new Date(t.dueDate)
+      return taskDate >= start && taskDate <= end
+    })
+  }
+  if (props.mode === 'trash'){
+    return trashTasks.value
+  }
+
+  return []
+})
+
+const showMainBlock = computed(() => {
+  return props.mode !== 'completed' && props.mode !== 'trash'
+})
+
 function saveTasks() {
   localStorage.setItem('tasks', JSON.stringify({
     incomplete: tasks.value,
-    complete: completedTasks.value
+    complete: completedTasks.value,
+    trash : trashTasks.value
   }))
 }
 
@@ -50,6 +117,7 @@ function fetchTasks() {
   const stored = JSON.parse(localStorage.getItem('tasks') || '{}')
   tasks.value = stored.incomplete || []
   completedTasks.value = stored.complete || []
+  trashTasks.value = stored.trash || []
 
   if (sortByDate.value) {
     tasks.value.sort(sortByDateFn)
@@ -82,6 +150,10 @@ function addTask() {
 }
 
 function deleteTask(taskId) {
+  const t = tasks.value.find(x => x.id === taskId) || completedTasks.value.find(x => x.id === taskId)
+  if (t) {
+    trashTasks.value.push({...t,deletedAt: new Date()})
+  }
   tasks.value = tasks.value.filter(t => t.id !== taskId)
   completedTasks.value = completedTasks.value.filter(t => t.id !== taskId)
   saveTasks()
@@ -117,18 +189,18 @@ function editDate(task, newDate) {
 }
 
 function completeTask(task) {
-  task.completed = true
+  const updatedTask = { ...task, completed: true }
   tasks.value = tasks.value.filter(t => t.id !== task.id)
-  completedTasks.value.push(task)
+  completedTasks.value.push(updatedTask)
   saveTasks()
 }
 function uncompleteTask(task) {
-  task.completed = false
+  const updatedTask = { ...task, completed: false }
   completedTasks.value = completedTasks.value.filter(t => t.id !== task.id)
-  tasks.value.push(task)
+  tasks.value.push(updatedTask)
   saveTasks()
-  fetchTasks()
 }
+
 
 function togglePopup(id, event) {
   taskMenu.value = taskMenu.value === id ? null : id
@@ -156,7 +228,6 @@ function handleSelectType(type) {
   fetchTasks()
 }
 
-
 function flagClass(priority) {
   switch (priority) {
     case 'red': return 'bx bx-flag text-red-500 text-xl'
@@ -171,8 +242,6 @@ function setPriority(task, color) {
   saveTasks()
   fetchTasks()
 }
-<<<<<<< HEAD
-=======
 function togglePin(task) {
   task.pinned = !task.pinned
   tasks.value.sort((a, b) => {
@@ -214,48 +283,46 @@ function deleteForever() {
 }
 
 const lists = ref([])
+
+function updateLists(updated) {
+  lists.value = updated
+  localStorage.setItem('myLists', JSON.stringify(lists.value))
+}
+
 onMounted(() => {
   const savedLists = localStorage.getItem('myLists')
   if (savedLists) lists.value = JSON.parse(savedLists)
 })
->>>>>>> parent of bc2ba8e (list)
 </script>
 
 <template>
   <div class="w-3/5 h-screen">
     <div class="flex flex-col p-6 ">
       <div class="f-center justify-between p-2 mb-2 ">
-        <p class="text-2xl font-black text-stone-600">Inbox</p>
-        <div class="text-stone-400 text-2xl">
+        <p  class="text-2xl font-black text-stone-600">     {{ pageTitle}}</p>
+        <div v-if="showMainBlock" class="text-stone-400 text-2xl">
           <button @click="toggleShuffle"><i class='bx  bx-shuffle'  ></i></button>
           <Taskshuffle v-if="showShuffle" class="absolute z-50" @selectType="handleSelectType"></Taskshuffle>
         </div>
       </div>
-      <div class="f-center bg-zinc-100 rounded-lg p-1 hover:bg-white border border-white focus-within:border-orange-300">
-        <input type="text" v-model="task" @keyup.enter="addTask" placeholder="+ Add task" class="w-3/5 bg-transparent text-slate-500 outline-none"/>
-        <i v-if="newTaskPriority" :class="flagClass(newTaskPriority)" class="w-1/5 ml-2"></i>
-        <input type="date" v-model="dueDate" locale="th" class="w-1/5 flex items-end ml-2 text-slate-500 bg-transparent outline-none border-none focus:ring-0" />
-        <div class="relative">
-          <button @click="(e) => togglePopup('new',e)">
-            <i class='bx  bx-chevron-down text-3xl'  ></i> 
-          </button>
-          <div v-if="taskMenu === 'new'" @click.self="closePopup" class="absolute z-99" 
-          :class="[popupPosition === 'top' ? 'bottom-full mb-2' : 'top-full mt-2', 'right-0']"> 
-            <TaskPopup isNew @set-priority="(color) => { newTaskPriority.value = color; closePopup(); }" />
-          </div>
-        </div>
+      <div v-if="showMainBlock " class="f-center justify-between bg-zinc-100 rounded-lg p-1 hover:bg-white border border-white focus-within:border-orange-300">
+        <input type="text" v-model="task" @keyup.enter="addTask" placeholder="+ Add task" class="bg-transparent text-slate-500 outline-none"/>
+        <input type="date" v-model="dueDate" locale="th" class="flex items-end ml-2 text-slate-500 bg-transparent outline-none border-none focus:ring-0" />
       </div>
     </div>
-    <div class="w-full h-3/5 overflow-y-auto pl-5 pr-14">
-      <div v-if="tasks.length" >
-        <draggable v-model="tasks" item-key="id" class="space-y-2 " handle=".drag-handle"   :animation="200" ghost-class="drag-ghost" chosen-class="drag-chosen">
+    <div v-if="showMainBlock " class="w-full h-3/6 overflow-y-auto pl-5 pr-14">
+      <div v-if="tasksToShow.length" >
+        <draggable :list="tasksToShow" item-key="id" class="space-y-2 " handle=".drag-handle"   :animation="200" ghost-class="drag-ghost" chosen-class="drag-chosen">
           <template #item="{ element: t }">
             <div class="w-full f-center">
               <button class="drag-handle icon-btn"><i class='bx bx-menu text-2xl'></i></button>
               <button @click="completeTask(t)"><i class='bx bx-checkbox checkbox'></i></button>
               <div class="task">
-                <span v-if="editID !== t.id" @click="edit(t)" class="w-full cursor-pointer select-none">{{ t.text }}</span>
-                <input v-else v-model="editText" class="w-full bg-transparent outline-none border-none focus:ring-0" @keyup.enter="editSave(t)" @blur="editSave(t)" ref="editInput"/>
+                <div class="flex items-center gap-4 ">
+                  <i v-if="t.pinned" class="bx bx-pin text-xl text-purple-400"></i>
+                  <span v-if="editID !== t.id" @click="edit(t)" class="w-full cursor-pointer select-none">{{ t.text }}</span>
+                  <input v-else v-model="editText" class="w-full bg-transparent outline-none border-none focus:ring-0" @keyup.enter="editSave(t)" @blur="editSave(t)" ref="editInput"/>
+                </div>
                 <div v-if ="t.dueDate && !isNaN(new Date(t.dueDate).getTime()) " class="w-full flex justify-end pr-4" >
                   {{ new Date(t.dueDate).toLocaleDateString('th-TH',{day:'numeric', month:'short',year:'numeric'}) }}</div>
                 <i v-if="t.priority" :class="flagClass(t.priority)"></i>
@@ -264,37 +331,68 @@ onMounted(() => {
                 <button @click="(e) => togglePopup(t.id,e)">
                   <i class='bx bx-dots-horizontal-rounded text-2xl text-zinc-300 ml-3'></i>
                 </button>
-<<<<<<< HEAD
-                <div v-if="taskMenu === t.id" @click.self="closePopup" class="absolute z-50" :class="popupPosition === 'top' ? 
-                'bottom-full mb-2' : 'top-full mt-2' ,'right-0'">
-                  <TaskPopup v-model="t.dueDate" @update:modelValue="(newDate) => editDate(t,newDate)" 
-                    @set-priority="(color) => setPriority(t, color)" @delete="deleteTask(t.id)"/>
-=======
                 <div v-if="taskMenu === t.id" @click.self="closePopup" class="absolute z-50 right-12" :class="[popupPosition === 'top' ? 
                 'bottom-full mb-2' : 'top-full mt-2' ,'right-0']">
                   <TaskPopup v-model="t.dueDate" @update:modelValue="(newDate) => editDate(t,newDate)"  @pin-task="togglePin(t)" 
-                    @set-priority="(color) => setPriority(t, color)" @delete="deleteTask(t.id)" :lists="lists"/>
->>>>>>> parent of bc2ba8e (list)
+                    @set-priority="(color) => setPriority(t, color)" @delete="deleteTask(t.id)" :lists="lists"  @update-lists="lists = $event"/>
                 </div>
               </div>
             </div>
           </template>
         </draggable>
       </div>
-      <div v-else class="f-center h-full justify-center ">
+      <div v-else class="flex flex-col text-center items-center justify-center w-full h-full">
             <img :src="emptytask" alt="Empty Inbox" class="w-64 h-auto object-contain opacity-50"/>
+            <h1 class="font-semibold text-stone-500">{{empty}}</h1>
+            <p class="text-stone-400">{{ emptydis }}</p>
       </div>
     </div>
-    <div class="pl-5">
-      <div class="f-center">
-        <i class='bx  bx-chevron-down text-2xl'  ></i> 
-        <h1>Completed</h1>
+    <div v-if="props.mode !== 'trash'" class="h-2/6 pl-5 overflow-y-auto">
+      <div class="f-center" @click="showCompleted = !showCompleted">
+        <i :class="showCompleted ? 'bx bx-chevron-down text-2xl' : 'bx bx-chevron-right text-2xl'"></i>
+        <h1>{{ showCompleted ? 'Hide Completed' : 'Show Completed' }} ({{ completedTasks.length }})</h1>
       </div>
-      <div v-if="completedTasks.length" class="space-y-2">
-        <div  v-for="t in completedTasks" :key="t.id" class="f-center" >
-          <button @click="uncompleteTask(t)"><i class='bx  bx-checkbox-checked checkbox'></i> </button>
-          <span class="line-through text-lg">{{ t.text }}</span>
+      <div v-show="showCompleted" class="space-y-2">
+        <draggable v-model="completedTasks" item-key="id" class="space-y-2" handle=".drag-handle"   
+          :animation="200" ghost-class="drag-ghost" chosen-class="drag-chosen">
+          <template #item="{ element: t }">
+            <div :key="t.id" class="f-center">
+              <button class="drag-handle icon-btn">
+                <i class='bx bx-menu text-2xl'></i>
+              </button>
+              <button @click="uncompleteTask(t)">
+                <i class='bx bx-checkbox-checked checkbox'></i>
+              </button>
+              <span class="line-through text-lg">{{ t.text }}</span>
+            </div>
+          </template>
+        </draggable>
+      </div>
+      <div v-if="props.mode == 'completed' && completedTasks.length === 0" class="flex flex-col text-center items-center justify-center w-full h-5/6">
+        <img :src="emptytask" alt="Empty Inbox" class="w-64 h-auto object-contain opacity-50"/>
+        <h1 class="font-semibold text-stone-500">{{empty}}</h1>
+        <p class="text-stone-400">{{ emptydis }}</p>
+      </div>
+    </div>
+    <div v-if="props.mode == 'trash'" class="flex flex-col space-y-2 w-full  p-5">
+      <div class="f-center justify-between" >
+        <div class="f-center gap-2">
+          <button @click="toggleselectTrash"><i :class="selectTrash ? 'bx bx-checkbox-checked checkbox' : 'bx bx-checkbox checkbox'"></i></button>
+          <p class="text-lg text-stone-500">All</p>
         </div>
+        <div class="f-center gap-2">
+          <button @click="restoreTasks"><i class='bx bx-undo text-3xl text-zinc-400'></i> </button>
+          <button @click="deleteForever"><i class='bx  bx-trash text-2xl text-zinc-400'  ></i> </button>
+        </div>
+      </div>
+      <div v-for="t in trashTasks" :key="t.id" class="f-center">
+        <button @click="t.isDeleted = !t.isDeleted"><i :class="t.isDeleted ? 'bx bx-checkbox-checked checkbox' : 'bx bx-checkbox checkbox'"></i></button>
+        <span class="text-lg text-stone-500">{{ t.text }}</span>
+      </div>
+      <div v-if="trashTasks.length === 0" class="flex flex-col text-center items-center justify-center w-full h-full">
+        <img :src="emptytask" alt="Empty Trash" class="w-64 h-auto object-contain opacity-50"/>
+        <h1 class="font-semibold text-stone-500">{{empty}}</h1>
+        <p class="text-stone-400">{{ emptydis }}</p>
       </div>
     </div>
   </div>
